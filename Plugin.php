aded4e3fb11506308b15caca59e2fe3c2dca64fa
Ronny John <me@ronnyjohnti.dev>
2024-01-25 17:01:59 -0300

    feat: Added registration user UI

--- Plugin.php
+++ Plugin.php
@@ -12,22 +12,54 @@ class Plugin extends \MapasCulturais\Plugin
     {
         $app = App::i();
 
-        $app->hook('template(opportunity.<<*>>):begin', function () use($app) {
-           $app->view->enqueueScript(
-               'app',
-               'swal2',
-               'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.all.min.js'
-           );
-           $app->view->enqueueStyle(
-               'app',
-               'swal2-theme-secultce',
-               'css/swal2-secultce.min.css'
-           );
-           $app->view->enqueueStyle(
-               'app',
-               'opinionManagement',
-               'css/opinionManagement.css'
-           );
+
+
+        $app->hook('template(<<registration|opportunity>>.<<*>>):begin', function () use ($app) {
+            $app->view->enqueueScript(
+                'app',
+                'swal2',
+                'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.all.min.js'
+            );
+            $app->view->enqueueStyle(
+                'app',
+                'swal2-theme-secultce',
+                'css/swal2-secultce.min.css'
+            );
+            $app->view->enqueueStyle(
+                'app',
+                'opinionManagement',
+                'css/opinionManagement.css'
+            );
+
+            $app->view->enqueueScript(
+                'app',
+                'opinion-management',
+                'OpinionManagement/js/opinionManagement.js'
+            );
+        });
+
+        $app->hook('template(panel.registrations.panel-header):before', function () use ($app) {
+            $app->view->enqueueScript(
+                'app',
+                'swal2',
+                'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.all.min.js'
+            );
+            $app->view->enqueueStyle(
+                'app',
+                'swal2-theme-secultce',
+                'css/swal2-secultce.min.css'
+            );
+            $app->view->enqueueStyle(
+                'app',
+                'opinionManagement',
+                'css/opinionManagement.css'
+            );
+
+            $app->view->enqueueScript(
+                'app',
+                'opinion-management',
+                'OpinionManagement/js/opinionManagement.js'
+            );
         });
 
         $app->hook('template(opportunity.<<create|edit|single>>.registration-list-header):end', function() use($app) {
@@ -40,11 +72,33 @@ class Plugin extends \MapasCulturais\Plugin
                 $this->part('OpinionManagement/registrations-table-column-admin.php');
 
                 $app->hook('template(opportunity.<<create|edit|single>>.registration-list-item):end', function() {
-                    $this->part('OpinionManagement/btn-show-opinion.php');
+                    $this->part('OpinionManagement/admin-btn-show-opinion.php');
                 });
 
-                $app->view->enqueueScript('app', 'opinion-management', 'OpinionManagement/js/opinionManagement.js');
+                $app->view->enqueueScript(
+                    'app',
+                    'opinion-management-tab-registrations',
+                    'OpinionManagement/js/admin-tab-registrations.js'
+                );
             }
+        });
+
+        $app->hook('template(registration.view.header-fieldset):after', function() use($app) {
+            $this->part('OpinionManagement/user-btn-show-opinion.php');
+            $app->view->enqueueScript(
+                'app',
+                'opinion-management',
+                'OpinionManagement/js/opinionManagement.js'
+            );
+        });
+
+        $app->hook('template(panel.registrations.panel-registration-meta):end', function ($registration) use ($app) {
+            $this->part('OpinionManagement/user-btn-show-opinion.php', ['registration' => $registration]);
+            $app->view->enqueueScript(
+                'app',
+                'opinion-management',
+                'OpinionManagement/js/opinionManagement.js'
+            );
         });
     }
 
--- assets/OpinionManagement/js/opinionManagement.js
+++ assets/OpinionManagement/js/opinionManagement.js
@@ -6,92 +6,62 @@ const handleChkCollapseChange = target => {
     }
 }
 
-$(document).ready(() => {
 
-    const opinionHtml = opinion => {
-        console.log(opinion)
-        let htmlParsed = '<div class="opinion">'
-        htmlParsed += `<div class="evaluation-title">
-            <h3>Parecerista <a href="${opinion.agent.singleUrl}" target="_blank">${opinion.agent.name}</a></h3>
-            <label for="chk-collapse-${opinion.id}"><div class="collapsible"></div></label>
-            <p>Resultado da avaliação documental:<a href="${opinion.singleUrl}" class="criteria-status-${opinion.result < 0 ? 'invalid' : 'valid'}"></a></p>
-        </div>
-        <input type="checkbox" id="chk-collapse-${opinion.id}" class="chk-collapse" name="chk-collapse" onchange="handleChkCollapseChange(this)">`
-        for(const criteriaId in opinion.evaluationData) {
-            if(criteriaId !== 'published') {
-                const criteria = opinion.evaluationData[criteriaId]
-                htmlParsed += `<div class="criteria-fields">`
-                htmlParsed += `<h5>${criteria.label}</h5>`
-                htmlParsed += `<p class="criteria-status-${criteria.evaluation === '' ? 'pending' : criteria.evaluation}"></p>`
-                htmlParsed += criteria.evaluation === 'invalid' ? `<p class="opinion-evaluation-obs">${criteria['obs_items']}</p>` : ''
-                htmlParsed += `</div>`
-            }
-        }
-        htmlParsed += '</div>'
-        return htmlParsed
-    }
-
-    const showOpinions = registrationId => {
-        fetch(MapasCulturais.baseURL + 'opinionManagement/opinions?'+ new URLSearchParams({
-            id: registrationId
-        }), {
-            headers: {
-                'Accept': 'application/json'
-            }
-        })
-            .then(response => {
-                console.log(response)
-                return response.json()
-            })
-            .then(opinions => {
-                console.log(opinions)
-                const html = `<div>${opinions.map(opinion => opinionHtml(opinion)).join('')}</div>`;
-
-                Swal.fire({
-                    html
-                })
-            })
-            .catch(error => {
-                Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Alconteceu um problema!",
-                    footer: `<code style="font-size:8px">${error}</code>`
-                })
-            })
-    }
-
-    // Promise criada para aguardar os elementos serem carregados na página
-    const waitLoadRegistrationList = () => {
-        return new Promise(resolve => {
-            const selector = '.showOpinion'
-
-            if(document.querySelectorAll(selector).length > 0) {
-                return resolve(document.querySelectorAll(selector))
-            }
-        })
-    }
-
-    // Esse observer observando mudanças no DOM da tabela de inscritos e chama a função que atribui o callback ao evento de clique aos botões de visualizar parecer
-    const observer = new MutationObserver(mutations => {
-        if(document.querySelectorAll('.showOpinion').length > 0) {
-            waitLoadRegistrationList().then(showOpinionButtons => {
-                for(const arrayIndex of showOpinionButtons.keys()) {
-
-                    showOpinionButtons[arrayIndex].onclick = e => {
-                        showOpinions(e.target.getAttribute('data-id'))
-                    }
-                }
-            })
+const opinionHtml = opinion => {
+    let htmlParsed = '<div class="opinion">'
+    htmlParsed += `<div class="evaluation-title">
+        <h3>Parecerista <a href="${opinion.agent.singleUrl}" target="_blank">${opinion.agent.name}</a></h3>
+        <label for="chk-collapse-${opinion.id}"><div class="collapsible"></div></label>
+        <p>Resultado da avaliação documental:<a href="${opinion.singleUrl}" class="criteria-status-${opinion.result < 0 ? 'invalid' : 'valid'}"></a></p>
+    </div>
+    <input type="checkbox" id="chk-collapse-${opinion.id}" class="chk-collapse" name="chk-collapse" onchange="handleChkCollapseChange(this)">`
+    for(const criteriaId in opinion.evaluationData) {
+        if(criteriaId === 'published') {
+            continue
         }
 
-        // @todo: Melhorar a validação para remover o MutationObserver caso todos os inscritos já carregaram na paǵina
-        if(document.querySelectorAll("tr[id^='#registration']").length % 50 !== 0) {
-            observer.disconnect()
+        const criteria = opinion.evaluationData[criteriaId]
+        criteria.obs_items = criteria.obs_items?.replace('\n','<br>')
+        criteria.obs = criteria.obs?.replace('\n','<br>')
+
+        htmlParsed += `<div class="criteria-fields">`
+        htmlParsed += `<h5>${criteria.label}</h5>`
+        htmlParsed += `<p class="criteria-status-${criteria.evaluation === '' ? 'pending' : criteria.evaluation}"></p>`
+        // htmlParsed += criteria.evaluation === 'invalid' ? `<p class="opinion-evaluation-obs">${criteria['obs_items']}</p>` : ''
+        htmlParsed += `<p class="opinion-evaluation-obs">${criteria['obs_items']}</p>`
+        htmlParsed += `<p class="opinion-evaluation-obs">${criteria['obs']}</p>`
+        htmlParsed += `</div>`
+    }
+    htmlParsed += '</div>'
+    return htmlParsed
+}
+
+const showOpinions = registrationId => {
+    fetch(MapasCulturais.baseURL + 'opinionManagement/opinions?'+ new URLSearchParams({
+        id: registrationId
+    }), {
+        headers: {
+            'Accept': 'application/json'
         }
     })
-    observer.observe(document.querySelector('#registrations-table'), {
-        childList: true,
-        subtree: true
-    })
-})
+        .then(response => {
+            console.log(response)
+            return response.json()
+        })
+        .then(opinions => {
+            console.log(opinions)
+            const html = `<div>${opinions.map(opinion => opinionHtml(opinion)).join('')}</div>`;
+
+            Swal.fire({
+                html
+            })
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Alconteceu um problema!",
+                footer: `<code style="font-size:8px">${error}</code>`
+            })
+        })
+}
